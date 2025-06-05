a02a4bfe825fab675f0df7122e422908383f3aaa
Dean <studentdean03@gmail.com>
2025-06-05 17:09:24 +0900

    mitask

--- train.js
+++ train.js
@@ -1,35 +1,35 @@
-console.log('jack ma maslahatlari')
-const list = [
-	'yaxshi talaba boling',
-	'togri boshliq tanlang koproq hato qiling',
-	'uzizngizni ishlaringizni boshlang',
-	'siz kuchli bolgan narsalarni qiling',
-	'yoshlarga investitsiya qiling',
-	'endi dam olin foydasi yoq',
-]
-//callback functionlar
-async function maslahatBering(a, callback) {
-	if (typeof a !== 'number') callback('insert a number', null)
-	else if (a <= 20) callback(null, list[0])
-	else if (a > 20 && a <= 30) callback(null, list[1])
-	else if (a > 30 && a <= 40) callback(null, list[2])
-	else if (a > 40 && a <= 50) callback(null, list[3])
-	else if (a > 50 && a <= 60) callback(null, list[4])
-	else {
-		setInterval(function () {
-			callback(null, list[5])
-		}, 1000)
-	}
-}
+// console.log('jack ma maslahatlari')
+// const list = [
+// 	'yaxshi talaba boling',
+// 	'togri boshliq tanlang koproq hato qiling',
+// 	'uzizngizni ishlaringizni boshlang',
+// 	'siz kuchli bolgan narsalarni qiling',
+// 	'yoshlarga investitsiya qiling',
+// 	'endi dam olin foydasi yoq',
+// ]
+// //callback functionlar
+// async function maslahatBering(a, callback) {
+// 	if (typeof a !== 'number') callback('insert a number', null)
+// 	else if (a <= 20) callback(null, list[0])
+// 	else if (a > 20 && a <= 30) callback(null, list[1])
+// 	else if (a > 30 && a <= 40) callback(null, list[2])
+// 	else if (a > 40 && a <= 50) callback(null, list[3])
+// 	else if (a > 50 && a <= 60) callback(null, list[4])
+// 	else {
+// 		setInterval(function () {
+// 			callback(null, list[5])
+// 		}, 1000)
+// 	}
+// }
 
-console.log('passed here 0')
-maslahatBering(70, (err, data) => {
-	if (err) console.log('ERROR', err)
-	else {
-		console.log(data)
-	}
-})
-console.log('passed here 1')
+// console.log('passed here 0')
+// maslahatBering(70, (err, data) => {
+// 	if (err) console.log('ERROR', err)
+// 	else {
+// 		console.log(data)
+// 	}
+// })
+// console.log('passed here 1')
 //async function defin qismida ishladdik
 // async function maslahatBering(a) {
 // 	if (typeof a !== 'number') throw new Error('insert a number')
@@ -76,3 +76,18 @@ console.log('passed here 1')
 // 	console.log(javob)
 // }
 // run()
+
+// A-TASK:
+
+function countLetter(letter, word) {
+	let count = 0
+	for (let i = 0; i < word.length; i++) {
+		if (word[i] === letter) {
+			count++
+		}
+	}
+	return count
+}
+
+console.log(countLetter('b', 'bee'))
+console.log(countLetter('e', 'dean'))
